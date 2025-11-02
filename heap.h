9a2c71b52715ce9a7591d82052e17a7b048b2750
ko545 <acadenaperales3860@sdsu.edu>
2025-11-02 11:02:50 -0800

    heap.h "complete"

--- heap.h
+++ heap.h
@@ -29,7 +29,7 @@ struct MinHeap {
     int pop(int weightArr[]) {
         if (size <= 0) {
             cout << "Empty Heap" << endl;
-            return 0;
+            return -1;
         }
         // TODO: remove and return smallest index
         //       Replace root with last element, then call downheap()
@@ -42,11 +42,30 @@ struct MinHeap {
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
-
+        while (pos > 0) {
+            int parent = (pos - 1) / 2;
+            if (weightArr[pos] >= weightArr[parent]) break; // heap property holds
+            swap(weightArr[pos], weightArr[parent]); //bubble up
+            pos = parent; // move up the tree
+        }
     }
 
     void downheap(int pos, int weightArr[]) {
         // TODO: swap parent downward while larger than any child
+        while (2 * pos + 1 < size) { //while left child exists
+            int left = 2 * pos + 1;
+            int right = 2 * pos + 2;
+            int smallest = left;
+
+            if (right < size && weightArr[right] < weightArr[left])
+                    smallest = right; // find smaller child
+
+            if (weightArr[pos] <= weightArr[smallest])
+                break; // heap property holds
+
+            swap(weightArr[pos], weightArr[smallest]); // bubble down
+            pos = smallest; // move down the tree
+        }
     }
 };
 
