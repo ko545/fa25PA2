0aee9888375ca049ae1bc5bc521ed3ff7a2e2436
ko545 <acadenaperales3860@sdsu.edu>
2025-11-02 10:24:27 -0800

    working on push, pop in heap.h

--- heap.h
+++ heap.h
@@ -9,23 +9,40 @@
 using namespace std;
 
 struct MinHeap {
-    int data[64];
-    int size;
+    int data[64]; // heap array ?
+    int size; // stores current size of the heap
 
     MinHeap() { size = 0; }
 
-    void push(int idx, int weightArr[]) {
-        // TODO: insert index at end of heap, restore order using upheap()
+    void push(int keyIndex, int weightArr[]) {
+        if (size >= 64) {
+            cout << "Full Heap" << endl;
+            return;
+        }
+        // TODO: insert index at end of heap
+        //       Restore order using upheap()
+        weightArr[size] = keyIndex;
+        upheap(size, weightArr);
+        size++;
     }
 
     int pop(int weightArr[]) {
+        if (size <= 0) {
+            cout << "Empty Heap" << endl;
+            return 0;
+        }
         // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        //       Replace root with last element, then call downheap()
+        int smallestIndex = weightArr[0]; // get smallest index
+        weightArr[0] = weightArr[size]; // replace root with last element
+        size--; //reduce size of array
+        downheap(size, weightArr);
+        return smallestIndex; // placeholder
     }
 
     void upheap(int pos, int weightArr[]) {
         // TODO: swap child upward while smaller than parent
+
     }
 
     void downheap(int pos, int weightArr[]) {
